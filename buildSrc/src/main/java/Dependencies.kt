a1d77a079b1867394e9cd0d49e05e2a18e2de711
BeokBeok <kekemusa37@nate.com>
2019-12-08 22:04:58 +0900

    kts 를 이용한 Dependency 정리

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,29 @@
+object Version {
+    const val KOTLIN = "1.3.61"
+    const val MATERIAL = "1.0.0"
+    const val CORE_KTX = "1.1.0"
+    const val CONSTRAINT_LAYOUT = "1.1.3"
+
+    const val JUNIT = "1.1.1"
+    const val ESPRESSO_CORE = "3.2.0"
+
+    const val FIREBASE_ANALYTICS = "17.2.1"
+    const val FIREBASE_AUTH = "19.2.0"
+    const val FIREBASE_UI_AUTH = "4.3.1"
+}
+
+object Libraries {
+    const val KOTLIN_STDLIB_JDK7 = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Version.KOTLIN}"
+    const val MATERIAL = "com.google.android.material:material:${Version.MATERIAL}"
+    const val CORE_KTX = "androidx.core:core-ktx:${Version.CORE_KTX}"
+    const val CONSTRAINT_LAYOUT =
+        "androidx.constraintlayout:constraintlayout:${Version.CONSTRAINT_LAYOUT}"
+
+    const val JUNIT = "androidx.test.ext:junit:${Version.JUNIT}"
+    const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Version.ESPRESSO_CORE}"
+
+    const val FIREBASE_ANALYTICS =
+        "com.google.firebase:firebase-analytics:${Version.FIREBASE_ANALYTICS}"
+    const val FIREBASE_AUTH = "com.google.firebase:firebase-auth:${Version.FIREBASE_AUTH}"
+    const val FIREBASE_UI_AUTH = "com.firebaseui:firebase-ui-auth:${Version.FIREBASE_UI_AUTH}"
+}
