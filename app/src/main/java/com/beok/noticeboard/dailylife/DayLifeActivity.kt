f3b242baed7e775e32ab056b6ddee916e4303737
BeokBeok <kekemusa37@nate.com>
2020-01-01 19:22:24 +0900

    [변경][레이아웃] "완료" 버튼이 있는 키패드

--- app/src/main/java/com/beok/noticeboard/dailylife/DayLifeActivity.kt
+++ app/src/main/java/com/beok/noticeboard/dailylife/DayLifeActivity.kt
@@ -1,6 +1,7 @@
 package com.beok.noticeboard.dailylife
 
 import android.os.Bundle
+import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.beok.noticeboard.R
@@ -13,6 +14,11 @@ class DayLifeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
+        setupUi()
+    }
+
+    private fun setupUi() {
+        binding.etDaylife.setRawInputType(InputType.TYPE_CLASS_TEXT)
     }
 
     private fun setupBinding() {
