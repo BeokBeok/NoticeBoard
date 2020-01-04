4ffb8065df809f4c339cac9d9e4912433eab6b13
BeokBeok <kekemusa37@nate.com>
2020-01-04 18:42:08 +0900

    [리펙토링] Glide 캐싱 처리

--- app/src/main/java/com/beok/noticeboard/wrapper/Glide.kt
+++ app/src/main/java/com/beok/noticeboard/wrapper/Glide.kt
@@ -4,6 +4,7 @@ import android.net.Uri
 import android.widget.ImageView
 import com.beok.noticeboard.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 object Glide {
 
@@ -19,6 +20,7 @@ object Glide {
         Glide.with(iv.context)
             .load(uri)
             .placeholder(R.mipmap.ic_launcher)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(iv)
     }
 }
